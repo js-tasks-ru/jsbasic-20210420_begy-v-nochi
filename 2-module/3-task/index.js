2b5dde7df3b398929d88459243e302e95d14c1a1
Anna Grischenkova <a_grischenkova@indatalabs.com>
2021-05-04 23:21:13 +0300

    fix 1-module 4-task add 2-module 3-task

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,10 @@
 let calculator = {
-  // ваш код
+  read: function(value, value2) {
+    this.firstNumber = value;
+    this.secondNumber = value2;
+  },
+  sum: function() { return this.firstNumber + this.secondNumber; },
+  mul: function() { return this.firstNumber*this.secondNumber; },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
